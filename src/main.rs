27fe1aa4bbb1842969809a5f80fa5cb16c3ca9ab
dbertella <daniele.bertella@gmail.com>
2019-11-25 12:38:38 +0000

    Improve implementation considering previous rota

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,76 @@
 use rand::seq::SliceRandom;
-use std::collections::HashMap;
 
 struct Reviewers<'a> {
-    pool_1: Vec<&'a str>,
-    pool_2: Vec<&'a str>,
-    review_count: HashMap<&'a str, usize>,
+    pool: Vec<Vec<&'a str>>,
 }
 
 impl<'a> Reviewers<'a> {
-    fn new(members: &'a [&'a str]) -> Self {
-        let mut review_count = HashMap::new();
-        for member in members.iter() {
-            review_count.insert(*member, 0);
-        }
-        Self {
-            pool_1: Vec::from(members),
-            pool_2: Vec::from(members),
-            review_count,
-        }
+    fn new(members: Vec<Vec<&'a str>>) -> Self {
+        Self { pool: members }
     }
 
-    fn get_first_reviewer(&mut self, member: &'a str) -> &'a str {
+    fn refresh_pool(&mut self, members: Vec<Vec<&'a str>>) {
+        self.pool = members;
+    }
+
+    fn get_reviewer(&mut self, member: &'a str) -> Result<Vec<&'a str>, String> {
         let mut rng = rand::thread_rng();
         loop {
-            self.pool_1.shuffle(&mut rng);
-            let reviewer = self.pool_1.last().unwrap();
+            self.pool.shuffle(&mut rng);
+            let current_choice = self.pool.last().unwrap();
 
-            if *self.review_count.get(reviewer).unwrap() == 1 || *reviewer == member {
-                self.pool_1.shuffle(&mut rng);
+            if current_choice.contains(&member) {
+                self.pool.shuffle(&mut rng);
             } else {
-                *self.review_count.get_mut(reviewer).unwrap() += 1;
-                return self.pool_1.pop().unwrap();
+                let mut new_list = self.pool.pop().unwrap();
+                new_list.push(member);
+                return Ok(new_list);
             }
         }
     }
 
-    fn get_second_reviewer(&mut self, members: [&'a str; 2]) -> &'a str {
-        let mut rng = rand::thread_rng();
-        loop {
-            self.pool_2.shuffle(&mut rng);
-            let reviewer = self.pool_2.last().unwrap();
-
-            if *self.review_count.get(reviewer).unwrap() == 2 || members.contains(reviewer) {
-                self.pool_2.shuffle(&mut rng);
-            } else {
-                *self.review_count.get_mut(reviewer).unwrap() += 1;
-                return self.pool_2.pop().unwrap();
-            }
+    fn build(&mut self, input: &Vec<Vec<&'a str>>) -> Vec<Vec<&'a str>> {
+        let mut members = Vec::new();
+        for row in input.iter() {
+            let updated_list = self.get_reviewer(row[0]);
+            members.push(updated_list.unwrap());
         }
+        members
     }
 }
 
 fn main() -> csv::Result<()> {
-    let data = [
-        "Oliver", "Olivia", "George", "Amelia", "Harry", "Ava", "Noah", "Isla", "Jack", "Emily",
-        "Leo", "Mia", "Arthur", "Isabella", "Muhammad", "Sophia", "Oscar", "Ella", "Charlie",
-        "Grace",
+    let input = vec![
+        vec!["Oliver", "Ella", "Charlie"],
+        vec!["Olivia", "Ava", "Ella"],
+        vec!["George", "Mia", "Muhammad"],
+        vec!["Amelia", "Muhammad", "Arthur"],
+        vec!["Harry", "Sophia", "Emily"],
+        vec!["Ava", "Charlie", "Oscar"],
+        vec!["Noah", "Isabella", "Mia"],
+        vec!["Isla", "Oscar", "Isabella"],
+        vec!["Jack", "Harry", "Ava"],
+        vec!["Emily", "Oliver", "Olivia"],
+        vec!["Leo", "Olivia", "Amelia"],
+        vec!["Mia", "Leo", "Grace"],
+        vec!["Arthur", "Jack", "Sophia"],
+        vec!["Isabella", "Emily", "Harry"],
+        vec!["Muhammad", "Amelia", "George"],
+        vec!["Sophia", "Noah", "Oliver"],
+        vec!["Oscar", "Grace", "Jack"],
+        vec!["Ella", "Isla", "Leo"],
+        vec!["Charlie", "George", "Isla"],
+        vec!["Grace", "Arthur", "Noah"],
     ];
 
-    let mut reviewers = Reviewers::new(&data);
-    let mut members = Vec::new();
-    for record in data.iter() {
-        let first_reviewer = reviewers.get_first_reviewer(record);
-        members.push((record, first_reviewer));
-    }
+    let mut reviewers = Reviewers::new(input.to_vec());
 
-    for (i, _) in data.iter().enumerate() {
-        let (member, first_reviewer) = members[i];
-        println!(
-            "{},{},{}",
-            member,
-            first_reviewer,
-            reviewers.get_second_reviewer([member, first_reviewer])
-        );
+    let mut members = reviewers.build(&input);
+    reviewers.refresh_pool(members);
+    members = reviewers.build(&input);
+
+    for m in members.iter() {
+        println!("{}, {}, {}", m[0], m[3], m[4]);
     }
     Ok(())
 }
